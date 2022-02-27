c4c8174274ab7c09193898dfe5cee13d48644630
bridge <maxbridge@foxmail.com>
2022-02-27 22:58:03 +0800

    feat:redis增删改查，设置过期、工具类、hash set list string 读写

--- spring-boot-redis/src/main/java/com/neo/config/RedisConfig.java
+++ spring-boot-redis/src/main/java/com/neo/config/RedisConfig.java
@@ -1,15 +1,12 @@
 package com.neo.config;
 
-import java.lang.reflect.Method;
-
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.core.RedisTemplate;
+
+import java.lang.reflect.Method;
 
 
 @Configuration
